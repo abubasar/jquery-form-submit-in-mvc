72f48b2cbf6128a5af27f39b72ee7c23e5765169
Rabbi <rabby.neo@outlook.com>
2019-01-21 14:07:32 +0600

    firstcommit

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+         
+            Dictionary<string, Employee> employeeList = new Dictionary<string,Employee>();
+            for (int i = 0; i < 5; i++)
+            {
+                employeeList.Add(Convert.ToString(i),new Employee());
+            }
+
+
+            EmployeeViewModel model = new EmployeeViewModel() { Employees = employeeList };
+            return View(model);
+        }
+        public ActionResult Post(EmployeeViewModel model)
+        {
+
+            List<Employee> employees = model.Employees.Select(x => x.Value).ToList();
+            return View();
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Assignment2/Models/EmployeeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public class EmployeeViewModel
+    {
+        public Dictionary<string, Employee> Employees { get; set; }
+
+        public EmployeeViewModel()
+        {
+            Employees = new Dictionary<string, Employee>();
+        }
+    }
+}
